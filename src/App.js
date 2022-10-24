a51c2dca5742a5d43560a536ed09be08394ca742
Vasyl Vasylyk <vvasylyk@star.global>
2022-10-24 14:16:56 +0200

    change server url

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { isEqual } from 'lodash';
 import { v4 } from 'uuid';
 import './App.css';
 
-export const serverUrl = process.env.NODE_ENV === 'development'
-	? 'http://localhost:3001'
-	: `${window.location.protocol}//${window.location.hostname}:${process.env.PORT}`;
+export const serverUrl = `${window.location.protocol}//${window.location.hostname}:${process.env.PORT || 3001}`;
 
 function App() {
 	const [messages, setMessages] = useState([]);
